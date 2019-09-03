330863c7e15eeafc33dda993832edb7f22fc3e6f
Komal-SkyNET <komal.venkateshg@gmail.com>
2019-09-03 12:12:58 +1000

    dfs traversal algorithm to permutation

--- recursions/permutations.py
+++ recursions/permutations.py
@@ -1,4 +1,4 @@
-ip = ["A", "B", "C", "D"]
+ip = ["A", "B", "C"]
 
 def perm(input, start):
     if(len(input)==start):
@@ -12,3 +12,29 @@ def perm(input, start):
         perm(ip_copy, start+1)
 
 perm(ip, 0)
+
+## for all elements in arr
+## idea is to pick say 'k' an element from the arr and it to the "PATH"
+## and then pass the remaining elements (i.e, arr removing k )
+
+""" example: 1,2,3
+    Whatever you pick add it to the path --
+    pick 1, path 1, arr[2,3]
+        pick 2, path 12, arr[3]
+            pick 3, path 123, arr[]
+        pick 3 - path 13 - arr[2]
+            pick 2, path 132, arr[]
+    pick 2.......and so on
+"""
+
+def dfs(arr, path):
+    
+    if not arr:
+        print path
+    else:
+        for i in xrange(len(arr)):
+            #arr[:i]+arr[i+1:] => exclude arr[i] from first argument to dfs recursion
+            #because we are adding it to the path
+            dfs(arr[:i]+arr[i+1:], path+str(arr[i]))
+
+dfs([1,2,3],"")
